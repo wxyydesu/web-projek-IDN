2a43ccb0bd7e6749c0b0d0ef8929a8c337d629f9
Verooo <125132435+vero57@users.noreply.github.com>
2023-10-30 15:07:06 +0700

    Add files via upload

---
+++ Movie90percent/koneksi database/sungutWebDaftar.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'sungutWebKoneksi.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $new_username = $_POST['username'];
+    $new_password = password_hash($_POST['password'], PASSWORD_DEFAULT); 
+
+    $stmt = $db->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+    if ($stmt->execute([$new_username, $new_password])) {
+        echo "Registrasi berhasil. Silakan login.";
+        header("location: loadingregister.html"); 
+    } else {
+        echo "Registrasi gagal. Username mungkin sudah digunakan.";
+        header("location: gagal.php"); 
+    }
+}
+?>
